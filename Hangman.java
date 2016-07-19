2c346b742d1b3d41ef79d4679f7e805fc08b1101
S <s>
2016-07-19 17:57:31 +0200

    {"types":"C","runs":19}

--- Hangman.java
+++ Hangman.java
@@ -47,19 +47,49 @@ public class Hangman extends ConsoleProgram {
     private void playGame(){
     	while(!endGameRequest){
 	    	pickOneWord();
-	    	while (!gameOver()){
-	    		guessLetterOrWord();
-	    		checkIfInTheWord();
-	    		putLetterInTheWord();
-	    		printTheWord();    		
+	    	setTheCurrentWordtoGuess();
+	    	while (!gameOver){
+	    		
+	    		promptForWord();
+	    		checkTypeOfInput();
+	    		if (checkIfApproperiate().equals("Char")){
+		    		putLetterInTheWord(checkIfCharInWord());
+	    		} else if (checkIfApproperiate().equals("String")){
+	    			checkIfProperGuessOfWord();
+	    			if (gameWon) break;
+	    		}
+	    		printTheGuessWord();   
+	    		checkIfGameOver();
 	    	}
+	    	printEndStatement();
+	    	setForTheNextGame();
+	    	
     	}
     }
     
-    private void gameOver(){
-    	if (lives == 0) gameOver = true;
+
+    /**
+     * sets the parameters ready for the next game
+     */
+    private void setForTheNextGame(){
+    	gameWon = false;
+    	gameOver = false;
+    	charTyped = "";
+    	word = "";
+    	currentWordToGuess = "";
+    	
+    }
+    private void checkIfGameOver(){
+    	if (lives == 0 || gameWon) {
+    		gameOver = true;
+    		lives = LIVES_NUMBER;
+    	}
+    	
     }
     
+    /**
+     * picks  one word from the chosen source and places it in field word
+     */
     private void pickOneWord(){
  
     	int pickWordNumber = rgen.nextInt(0, lexicon.getWordCount());
@@ -67,18 +97,171 @@ public class Hangman extends ConsoleProgram {
     }
     
     /**
-     * prompts to type the guess letter or word, checks if it is approperiate and changes the guessWord or guessLetter
+     * sets the currentWordToGuess as approperiate number of underscores each followed by space
+     *REMEMBER THAT TO CHANGE BACKSLASH YOU MUST REFER TO BACKSLASH SEPARATED BY A SPACE!
      */
-    private void guessLetterOrWord(){
-    	String currentGuessString = readLine("Please type a guess letter or a guess word");
-    	
+    private void setTheCurrentWordtoGuess(){
+    	for (int i=0; i < word.length(); i++){
+    		currentWordToGuess += "_ ";
+    	}
     	
     }
     
+    /**
+     * checks if the typed input is approperiate for the game
+     * and changes the guessWord or guessLetter to store the processed Word/Character
+     * or throws an exception if inapproperiate input
+     */
+    private void checkTypeOfInput(){
+    	
+    	if (checkIfApproperiate().equals("Inapproperiate")){
+    		throwException();
+    	} else if (checkIfApproperiate().equals("Char")){
+    		convertToChar();
+    	} else if (checkIfApproperiate().equals("Word")){
+    		guessWord = currentGuessString;
+    	}    	
+    }
+/**
+ * informs the user that he typed an inapproperiate data type
+ * and instructs what input is correct
+ */
+    private void throwException(){
+    	println("Inapproperiate input");
+    	println("Please write only a character or a whole word without numbers, special letters or spaces");
+    }
+   
+    /**
+     * assigns the guessed character to a guessLetter
+     */
+    private void convertToChar(){
+    	guessLetter = currentGuessString.charAt(0);
+    }
+    
+    /**
+     * Assign the typed word to the currentGuessString
+     */
+    private void promptForWord(){
+    	currentGuessString = readLine("Please type a guess letter or a guess word");
+    }
+
+    /**
+     * checks if the typed string is approperiate for the given job (is a letter or a string)
+     * @return "Word" if an approperiate word, "Char" if an approperiate char and "Inapproperiate" if wrong input
+     */
+    private String checkIfApproperiate(){
+    	if (currentGuessString.length() == 0){
+    		char firstChar = currentGuessString.charAt(0);
+    		if (Character.isLetter(firstChar)) return "Char";
+    	} else if(haveOnlyLetters()){
+    		return "Word";
+    	} 
+    	else return "Inapproperiate";
+    	return ""; /*only for the logic of the complier*/
+    }
+ 
+    /**
+     * checks if the typed string has only letters
+     * @return true if yes, false if no
+     * preconditions: currentGuessString is a string
+     */
+    private boolean haveOnlyLetters(){
+    	for (int i =0; i < currentGuessString.length(); i++){
+    		char currentChar = currentGuessString.charAt(i);
+    		if (!Character.isLetter(currentChar)) {
+    			return false;
+    		}
+    	}
+    	return true ;
+    }
+    
+    /**
+     * checks if the picked character is within the word
+     * @return boolean
+     */
+    private boolean checkIfCharInWord(){
+    	for (int i = 0; i < word.length(); i++)
+    	if (word.charAt(i) == guessLetter) return true;
+    return false;
+    }
+    
+    /**
+     * Replaces the guessLetter in every place in the currentWordToGuess 
+     * that it appears in the word (to guess)
+     * Puts the typed char into a charTyped
+     * @param charInWord (boolean) specifies if the guessed character is in the word
+     */
+    private void putLetterInTheWord(boolean charInWord){
+    	if (charInWord){
+    		for (int i = 0; i < word.length(); i++)
+    	    	if (word.charAt(i) == guessLetter){
+    	    		currentWordToGuess = currentWordToGuess.substring(0, i*2-2) 
+    	    				+ guessLetter + currentWordToGuess.substring(i);
+    	    	}
+    	} else if (!letterAlreadyTyped()){
+    		lives--;
+    	}
+    	charTyped+=guessLetter;
+    }
+    
+    /**
+     * checks if the letter typed was already typed beforehand
+     * @return true or false
+     */
+    private boolean letterAlreadyTyped(){
+    	
+  
+	    for (int i = 0; i < charTyped.length(); i++){
+			if (charTyped.charAt(i) == guessLetter){
+				println("Letter already checked");
+	    		return true;
+			}
+	    }
+	    return false;
+	}
+    /**
+     * checks if the game is won. If so, changes gameWon to true.
+     * If not takes off a life (the guess word was not proper)
+     */
+    private void checkIfProperGuessOfWord(){
+    	if (guessWord.equals(word)) gameWon = true;
+    	else lives--;
+    }
+    
+    /**
+     * prints the current state of the word to guess in the statement
+     */
+    private void printTheGuessWord(){
+    	println("The current state of guess is: " + currentWordToGuess);
+    }
+    
+    private void printEndStatement(){
+    	if (gameOver){
+    		println("You Lose");
+    		
+    	} else if (gameWon){
+    		println("Congratulations");
+    		println("YOU WON");
+    	}
+    	println("To play again hit spacebar");
+		println("");
+    }
+    
+    
+    
     RandomGenerator rgen = RandomGenerator.getInstance();
+    
     private HangmanLexicon lexicon;
     private static char guessLetter;
-    private static String word, guessWord;
-    private static boolean endGameRequest = false, gameOver = false;
+    
+    /**
+     * word - word to be guessed, 
+     * currentWordToGuess - word that is typed as an input
+     * guessWord - word that is typed as an input, 
+     * currentGuessString - string recently typed as an input
+     * charTyped - a string with all the characters previously typed during the game
+     */
+    private static String word, currentWordToGuess = "", guessWord = "", currentGuessString = "", charTyped = "";
+    private static boolean endGameRequest = false, gameOver = false, gameWon=false;
     private static int lives = LIVES_NUMBER;
 }
