57aad9e4f6f744f7467287e628c75bf1120998a0
S <s>
2016-07-19 21:13:16 +0200

    {"types":"C","runs":58}

--- Hangman.java
+++ Hangman.java
@@ -53,6 +53,7 @@ public class Hangman extends ConsoleProgram {
 	    		promptForWord();
 	    		checkTypeOfInput();
 	    		actOnInputLetter();
+	    		if (gameWon) break;
 	    		checkIfGameOver();
 	    		printTheGuessWord();   
 	    		
